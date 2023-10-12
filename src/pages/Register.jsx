22fedaf1a072b00155c2dcde42939e28b18e9fb9
Kamil Lewiński <61652256+Lewissimo@users.noreply.github.com>
2023-10-12 22:36:02 +0200

    sending data

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,38 @@
 import React, { useState } from "react";
 import Add from "../img/addAvatar.png";
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firebase';
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value)
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    // const file = e.target[3].value;
+
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
 
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Lama Chat</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="display name" />
           <input required type="email" placeholder="email" />
           <input required type="password" placeholder="password" />
